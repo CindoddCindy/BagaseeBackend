e133030ab479bf58454bca07d68392a1a501b720
CindoddCindy <cindodcindy@gmail.com>
2020-10-05 18:06:26 +0700

    test push

---
+++ cindodcindySpring/bagasee/src/main/java/sirihpinang/bagasee/BagaseeApplication.java
@@ -0,0 +1,13 @@
+package sirihpinang.bagasee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagaseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagaseeApplication.class, args);
+	}
+
+}
---
+++ cindodcindySpring/bagasee/src/test/java/sirihpinang/bagasee/BagaseeApplicationTests.java
@@ -0,0 +1,13 @@
+package sirihpinang.bagasee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagaseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
