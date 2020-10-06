d6c7be41d11d9b7a950ed9888d25094299443152
CindoddCindy <cindodcindy@gmail.com>
2020-10-06 08:45:49 +0700

    seller class baru set get

--- cindodcindySpring/bagasee/src/main/java/sirihpinang/bagasee/model/seller/SellerBaggage.java
+++ cindodcindySpring/bagasee/src/main/java/sirihpinang/bagasee/model/seller/SellerBaggage.java
@@ -52,7 +52,114 @@ public class SellerBaggage{
 
     @Column(name="seller_harga_bagasi")
     private String seller_harga_bagasi;
-    
+
+
+    public SellerBaggage(){
+
+    }
+
+    public void setSellerId(Long seller_id){
+        this.seller_id=seller_id;
+    }
+
+    public Long getSellerId(){
+        return seller_id;
+    }
+
+    public void setSellerName(String seller_name){
+        this.seller_name=seller_name;
+    }
+
+    public String getSellerName(){
+        return seller_name;
+    }
+
+    public void setSellerTanggalBerangkat(){
+        this.seller_tanggal_berangkat=seller_tanggal_berangkat;
+    }
+
+    public String getSellerTanggalBerangkat(){
+        return seller_tanggal_berangkat;
+    }
+
+    public void setSellerJamBerangkat(String seller_jam_berangkat){
+        this.seller_jam_berangkat=seller_jam_berangkat;
+    }
+    public String getSellerJamBerangkat(){
+        return seller_jam_berangkat;
+    }
+
+    public void setSellerTanggalTiba(String seller_tanggal_tiba){
+        this.seller_tanggal_tiba=seller_tanggal_tiba;
+    }
+
+    public String getSellerTanggalTiba(){
+        return seller_tanggal_tiba;
+    }
+
+    public void setSellerJamTiba(String seller_jam_tiba){
+        this.seller_jam_tiba=seller_jam_tiba;
+    }
+
+    public String getSellerJamTiba(){
+        return seller_jam_tiba;
+    }
+
+    public void setSellerBerangkatDari(String seller_berangkat_dari){
+        this.seller_berangkat_dari=seller_berangkat_dari;
+    }
+
+    public String getSellerBerangkatDari(){
+        return seller_berangkat_dari;
+    }
+
+    public void setSellerBerangkatKe(String seller_berangkat_ke){
+        this.seller_berangkat_ke=seller_berangkat_ke;
+    }
+
+    public String getSellerBerangkatKe(){
+        return seller_berangkat_ke;
+    }
+
+    public void setSellerJenisTransportasi(String seller_jenis_transportasi){
+        this.seller_jenis_transportasi=seller_jenis_transportasi;
+    }
+
+    public String getSellerJenisTransportasi(){
+        return seller_jenis_transportasi;
+    }
+
+    public void setSellerNamaTransportasi(String seller_nama_transportasi){
+        this.seller_nama_transportasi=seller_nama_transportasi;
+    }
+
+    public String getSellerNamaTransportasi(){
+        return seller_nama_transportasi;
+    }
+
+    public void setSellerJumlahBagasi(String seller_jumlah_bagasi){
+        this.seller_jumlah_bagasi=seller_jumlah_bagasi;
+    }
+
+    public String getSellerJumlahBagasi(){
+        return seller_jumlah_bagasi;
+    }
+
+    public void setSellerJenisBarang(String seller_jenis_barang){
+        this.seller_jenis_barang=seller_jenis_barang;
+    }
+
+    public String getSellerJenisBarang(){
+        return seller_jenis_barang;
+    }
+
+    public void setSellerHargaBagasi(String seller_harga_bagasi){
+        this.seller_harga_bagasi=seller_harga_bagasi;
+    }
+
+    public String getSellerHargaBagasi(){
+        return seller_harga_bagasi;
+    }
 
 
 }
